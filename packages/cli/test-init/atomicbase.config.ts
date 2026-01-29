d0104cf6d2b1ac56ac2d0709e44a86b3922fd327
Joe Ervin <joeerv123@outlook.com>
2026-01-29 18:27:01 -0500

    removed output from configs

--- packages/cli/test-init/atomicbase.config.ts
+++ packages/cli/test-init/atomicbase.config.ts
@@ -4,5 +4,4 @@ export default defineConfig({
   url: process.env.ATOMICBASE_URL || "http://localhost:8080",
   apiKey: process.env.ATOMICBASE_API_KEY,
   schemas: "./schemas",
-  output: "./schemas",
 });
